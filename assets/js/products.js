5747c5c3c26067713d819514c34aa6b420680de3
courtonepickleball <disnis3@gmail.com>
2026-06-24 17:10:14 +0900

    Add files via upload

---
+++ assets/js/products.js
@@ -0,0 +1,461 @@
+window.PRODUCTS = [
+  {
+    id: 'FPANNA-LEIGH-WATERS_SIGNATUREA16WH',
+    name: 'Franklin Paddle ANNA-LEIGH-WATERS_SIGNATURE Aurelious 16mm white',
+    price: 510000,
+    image: './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA16WH.webp',
+    images: [
+      './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA16WH.webp',
+      './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA16WH1.webp',
+      './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA16WH2.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPANNA-LEIGH-WATERS_SIGNATUREA14WH',
+    name: 'Franklin Paddle ANNA-LEIGH-WATERS_SIGNATURE Aurelious 14mm white',
+    price: 510000,
+    image: './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA14WH.webp',
+    images: [
+      './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA14WH.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPANNA-LEIGH-WATERS_SIGNATUREA12.7WH',
+    name: 'Franklin Paddle ANNA-LEIGH-WATERS_SIGNATURE Aurelious 12.7mm white',
+    price: 510000,
+    image: './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA12.7WH.webp',
+    images: [
+      './assets/images/FPANNA-LEIGH-WATERS_SIGNATUREA12.7WH.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FPPARRIS-TODD_C45S13.25HO',
+    name: 'Franklin Paddle PARRIS-TODD_C45 Series 13.25mm hotpink-surt blue',
+    price: 410000,
+    image: './assets/images/FPPARRIS-TODD_C45S13.25HO.webp',
+    images: [
+      './assets/images/FPPARRIS-TODD_C45S13.25HO.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBONH14LA',
+    name: 'Franklin Paddle C45-CARBON Hayden-Series 14mm laguna',
+    price: 410000,
+    image: './assets/images/FPC45-CARBONH14LA.webp',
+    images: [
+      './assets/images/FPC45-CARBONH14LA.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBONT16RE',
+    name: 'Franklin Paddle C45-CARBON Tempo 16mm red',
+    price: 410000,
+    image: './assets/images/FPC45-CARBONT16RE.webp',
+    images: [
+      './assets/images/FPC45-CARBONT16RE.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBONT14RE',
+    name: 'Franklin Paddle C45-CARBON Tempo 14mm red',
+    price: 410000,
+    image: './assets/images/FPC45-CARBONT14RE.webp',
+    images: [
+      './assets/images/FPC45-CARBONT14RE.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBOND16MI',
+    name: 'Franklin Paddle C45-CARBON Dynasty 16mm mint',
+    price: 410000,
+    image: './assets/images/FPC45-CARBOND16MI.webp',
+    images: [
+      './assets/images/FPC45-CARBOND16MI.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBOND16RE',
+    name: 'Franklin Paddle C45-CARBON Dynasty 16mm red',
+    price: 410000,
+    image: './assets/images/FPC45-CARBOND16RE.webp',
+    images: [
+      './assets/images/FPC45-CARBOND16RE.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBOND14MI',
+    name: 'Franklin Paddle C45-CARBON Dynasty 14mm mint',
+    price: 410000,
+    image: './assets/images/FPC45-CARBOND14MI.webp',
+    images: [
+      './assets/images/FPC45-CARBOND14MI.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPC45-CARBOND14RE',
+    name: 'Franklin Paddle C45-CARBON Dynasty 14mm red',
+    price: 410000,
+    image: './assets/images/FPC45-CARBOND14RE.webp',
+    images: [
+      './assets/images/FPC45-CARBOND14RE.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURD16BL',
+    name: 'Franklin Paddle FS-TOUR Dynasty 16mm blue',
+    price: 280000,
+    image: './assets/images/FPFS-TOURD16BL.webp',
+    images: [
+      './assets/images/FPFS-TOURD16BL.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURD16PI',
+    name: 'Franklin Paddle FS-TOUR Dynasty 16mm pink',
+    price: 280000,
+    image: './assets/images/FPFS-TOURD16PI.webp',
+    images: [
+      './assets/images/FPFS-TOURD16PI.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURD14PI',
+    name: 'Franklin Paddle FS-TOUR Dynasty 14mm pink',
+    price: 280000,
+    image: './assets/images/FPFS-TOURD14PI.webp',
+    images: [
+      './assets/images/FPFS-TOURD14PI.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPFS-TOUR_TODD_EDDTIONT12BL',
+    name: 'Franklin Paddle FS-TOUR_TODD_EDDTION Tempo 12mm blue',
+    price: 280000,
+    image: './assets/images/FPFS-TOUR_TODD_EDDTIONT12BL.webp',
+    images: [
+      './assets/images/FPFS-TOUR_TODD_EDDTIONT12BL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURT12WH',
+    name: 'Franklin Paddle FS-TOUR Tempo-Featherweight 12mm white',
+    price: 280000,
+    image: './assets/images/FPFS-TOURT12WH.webp',
+    images: [
+      './assets/images/FPFS-TOURT12WH.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURT14BL',
+    name: 'Franklin Paddle FS-TOUR Tempo 14mm blue',
+    price: 280000,
+    image: './assets/images/FPFS-TOURT14BL.webp',
+    images: [
+      './assets/images/FPFS-TOURT14BL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FPFS-TOURT16GR',
+    name: 'Franklin Paddle FS-TOUR Tempo 16mm gray',
+    price: 280000,
+    image: './assets/images/FPFS-TOURT16GR.webp',
+    images: [
+      './assets/images/FPFS-TOURT16GR.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPSIGNATURE-CARBONE13BL',
+    name: 'Franklin Paddle SIGNATURE-CARBON Elongated 13mm blue',
+    price: 179000,
+    image: './assets/images/FPSIGNATURE-CARBONE13BL.webp',
+    images: [
+      './assets/images/FPSIGNATURE-CARBONE13BL.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPSIGNATURE-CARBONT13BL',
+    name: 'Franklin Paddle SIGNATURE-CARBON Traditional 13mm blue',
+    price: 179000,
+    image: './assets/images/FPSIGNATURE-CARBONT13BL.webp',
+    images: [
+      './assets/images/FPSIGNATURE-CARBONT13BL.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPSIGNATURE-FIBERGLASSM13BL',
+    name: 'Franklin Paddle SIGNATURE-FIBERGLASS Maxgrit 13mm blue',
+    price: 139000,
+    image: './assets/images/FPSIGNATURE-FIBERGLASSM13BL.webp',
+    images: [
+      './assets/images/FPSIGNATURE-FIBERGLASSM13BL.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPSIGNATURE-FIBERGLASSM16BL',
+    name: 'Franklin Paddle SIGNATURE-FIBERGLASS Maxgrit 16mm blue',
+    price: 139000,
+    image: './assets/images/FPSIGNATURE-FIBERGLASSM16BL.webp',
+    images: [
+      './assets/images/FPSIGNATURE-FIBERGLASSM16BL.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FPSIGNATURE-FIBERGLASSM16WH',
+    name: 'Franklin Paddle SIGNATURE-FIBERGLASS Maxgrit 16mm white-gold',
+    price: 139000,
+    image: './assets/images/FPSIGNATURE-FIBERGLASSM16WH.webp',
+    images: [
+      './assets/images/FPSIGNATURE-FIBERGLASSM16WH.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FOX-40B100-PACK-BOXOP',
+    name: 'Franklin Outdoor-Ball X-40 Ball 100-pack-box optic',
+    price: 460000,
+    image: './assets/images/FOX-40B100-PACK-BOXOP.webp',
+    images: [
+      './assets/images/FOX-40B100-PACK-BOXOP.webp'
+    ],
+    featured: true,
+    special: true,
+    active: true
+  },
+  {
+    id: 'FOX-40B6PACKNE',
+    name: 'Franklin Outdoor-Ball X-40 Ball 6pack neon',
+    price: 30000,
+    image: './assets/images/FOX-40B6PACKNE.webp',
+    images: [
+      './assets/images/FOX-40B6PACKNE.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FBPROP1EABL',
+    name: 'Franklin Bag PRO Pickleball-Bag 1eamm black-gold',
+    price: 200000,
+    image: './assets/images/FBPROP1EABL.webp',
+    images: [
+      './assets/images/FBPROP1EABL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FBFLEXP1EAPI',
+    name: 'Franklin Bag FLEX Pickleball-Bag 1ea pink',
+    price: 160000,
+    image: './assets/images/FBFLEXP1EAPI.webp',
+    images: [
+      './assets/images/FBFLEXP1EAPI.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FBPICKLEBALLS1EABL',
+    name: 'Franklin Bag PICKLEBALL Sling-Bag 1ea black-optic-yellow',
+    price: 80000,
+    image: './assets/images/FBPICKLEBALLS1EABL.webp',
+    images: [
+      './assets/images/FBPICKLEBALLS1EABL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FBPROP1EAWH',
+    name: 'Franklin Bag PRO Pickleball-Bag 1ea white-gold',
+    price: 200000,
+    image: './assets/images/FBPROP1EAWH.webp',
+    images: [
+      './assets/images/FBPROP1EAWH.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'KALENS-FREE PICKLEBALLECLASSIS-FITBL',
+    name: 'Kitchen-Blockers Accessories LENS-FREE PICKLEBALL Eye-Protection classis-fit black',
+    price: 125000,
+    image: './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITBL.webp',
+    images: [
+      './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITBL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'KALENS-FREE PICKLEBALLECLASSIS-FITRE',
+    name: 'Kitchen-Blockers Accessories LENS-FREE PICKLEBALL Eye-Protection classis-fit red',
+    price: 125000,
+    image: './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITRE.webp',
+    images: [
+      './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITRE.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'KALENS-FREE PICKLEBALLECLASSIS-FITSK',
+    name: 'Kitchen-Blockers Accessories LENS-FREE PICKLEBALL Eye-Protection classis-fit sky-blue',
+    price: 125000,
+    image: './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITSK.webp',
+    images: [
+      './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITSK.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'KALENS-FREE PICKLEBALLECLASSIS-FITPI',
+    name: 'Kitchen-Blockers Accessories LENS-FREE PICKLEBALL Eye-Protection classis-fit pink',
+    price: 125000,
+    image: './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITPI.webp',
+    images: [
+      './assets/images/KALENS-FREE PICKLEBALLECLASSIS-FITPI.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'KALENS-FREE PICKLEBALLESLIM-FITCL',
+    name: 'Kitchen-Blockers Accessories LENS-FREE PICKLEBALL Eye-Protection slim-fit clear',
+    price: 125000,
+    image: './assets/images/KALENS-FREE PICKLEBALLESLIM-FITCL.webp',
+    images: [
+      './assets/images/KALENS-FREE PICKLEBALLESLIM-FITCL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FAPADDLE-MAGICE1EABL',
+    name: 'Franklin Accessories PADDLE-MAGIC Eraser 1ea black',
+    price: 20000,
+    image: './assets/images/FAPADDLE-MAGICE1EABL.webp',
+    images: [
+      './assets/images/FAPADDLE-MAGICE1EABL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FAPADDLEC1EABL',
+    name: 'Franklin Accessories PADDLE Cover 1ea black-gold',
+    price: 15300,
+    image: './assets/images/FAPADDLEC1EABL.webp',
+    images: [
+      './assets/images/FAPADDLEC1EABL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  },
+  {
+    id: 'FAPREMIUM-PERFORMANCES1EABL',
+    name: 'Franklin Accessories PREMIUM-PERFORMANCE Stretch-Hat 1ea black',
+    price: 30000,
+    image: './assets/images/FAPREMIUM-PERFORMANCES1EABL.webp',
+    images: [
+      './assets/images/FAPREMIUM-PERFORMANCES1EABL.webp'
+    ],
+    featured: true,
+    special: false,
+    active: true
+  }
+];
+
+window.CART_CODES = [
+  'C4PQ','P267','3KL6','7380','75TB','GE17','MXS2','OHU7','TJ96','2TC1',
+  '7515','TBNO','68DP','H04R','2S83','O354','38M7','297C','0E2F','91WK',
+  'TO38','0QK3','C8S2','C6Q9','24EX','1S46','2FEW','BGPX','IDBG','S9GP',
+  '79V3','3V18','Q5F9','5RMT','0QCR','DOER','ZW33','1942','6195','4BO4',
+  '69E4','5IR8','Z436','SO95','JYW7','92M5','7T3O','7EGP','8N5U','CHF4',
+  '76RF','IS8M','59P7','323A','604Z','CES7','HN4V','2N58','IK0H','4WI6',
+  'WR28','409F','889J','9YHV','Q7BI','UPK6','F0RT','T8FT','2592','0B39',
+  'P4J8','85M6','C920','F8A6','1XB7','7680','HWZ0','P247','0O9A','1J20',
+  '6Z37','9EUH','0NS9','S446','5URT','IR0U','HGDM','H22K','1BNJ','0F43',
+  'GNW3','3X15','5E49','82T7','4A8K','U148','DHER','6149','F5PD','CEKI'
+];
